305b1e5902cecaa559319733616b16c2a6c80224
chani <click0548450396@gmail.com>
2018-01-07 13:54:17 +0200

    1

---
+++ client/App.js
@@ -0,0 +1,38 @@
+(function() {
+
+  "use strict";
+
+  var App = angular.module("App", [
+    "App.controllers",
+    "App.services",
+    "App.directives",
+    "App.filters",
+    "ngRoute",
+    "ngResource"
+  ]);
+
+  App.config(function ($routeProvider) {
+    $routeProvider
+      .when('/home', {
+           templateUrl: 'view/home/home.html'
+      })
+      .when('/contact', {
+           templateUrl: 'view/contact/contact.html'
+      })
+      .when('/user', {
+        templateUrl: 'view/costumer/costumer.html'
+      })
+      .when('/order', {
+        templateUrl: 'view/order/order.html'
+      })
+      .when('/book', {
+        templateUrl: 'view/flipbook/flipbook.html'
+      })
+      .when('/magazines', {
+        templateUrl: 'view/papers/papers.html'
+      })
+      
+      .otherwise({redirectTo : 'home'});
+  });
+
+}());
