1f41960fca23880cf27e528cd5a0875c1624385c
chani <click0548450396@gmail.com>
2018-01-22 00:43:43 +0200

    dsasad

--- client/view/order/order.controller.js
+++ client/view/order/order.controller.js
@@ -1,4 +1,43 @@
  App.controller("OrderCtrl", function($scope) {
-    $scope.chani = 'Chani!!';
+     $scope.chek = [{
+             name: "lainyan",
+             nameH: "לעניין"
+         },
+         {
+             name: "meida",
+             nameH: "מידע-לכל"
+         },
+         {
+             name: "emtza",
+             nameH: "אמצע השבוע"
+         },
+         {
+             name: "shavua",
+             nameH: "שבוע טוב"
+         },
 
-    });
+     ];
+
+     $scope.size = [
+         "עמוד שלם",
+         "דבל",
+         "חצי עמוד רוחב",
+         "חצי עמוד גובה",
+         "רבע עמוד גובה",
+         "רבע עמוד רוחב",
+         "שמינית עמוד",
+         "1/16 (בלוח)",
+         "1/32 (בלוח)",
+         "קוביה בעמוד ראשון",
+     ];
+
+
+
+
+
+
+
+
+
+
+ });
